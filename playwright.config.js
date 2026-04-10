e8bcec6994ca8666a092c56ef2d4f3f671bf16e7
KokiHonjo <koki_honjo@dsd.dokidoki-gw.com>
2026-04-10 13:51:38 +0900

    anything

--- playwright.config.js
+++ playwright.config.js
@@ -3,22 +3,24 @@ const { defineConfig } = require('@playwright/test');
 module.exports = defineConfig({
   testDir: './tests',
   timeout: 30000,
-  retries: 2,
   reporter: [['list'], ['html']],
   use: {
     headless: true,
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
+    trace: 'retain-on-failure',
   },
   projects: [
     {
       name: 'normal-chromium',
       testMatch: /normal\/.*\.spec\.js$/,
+      retries: 2,
       use: { browserName: 'chromium' },
     },
     {
       name: 'bug-chromium',
       testMatch: /bug\/.*\.spec\.js$/,
+      retries: 0,
       use: { browserName: 'chromium' },
     },
   ],
